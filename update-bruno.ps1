559a29e91d67bd6899b8be37c7c8cb072c31ef61
Denis Baumgärtner <denco@freenet.de>
2025-12-15 16:37:37 +0100

    add new tools

---
+++ update-bruno.ps1
@@ -0,0 +1,97 @@
+# [CmdletBinding()]
+[cultureinfo]::CurrentUICulture='en-US'
+
+$TargetPath = "$($env:LOCALAPPDATA)\Programs"
+
+$7Zip = "$($TargetPath)\7-Zip\7z.exe"
+
+$vScanner = "${env:ProgramFiles}\Windows Defender\MpCmdRun.exe"
+
+$ToolName = "bruno"
+$ToolUpdateDelayDays = 0
+$TargetDir = "$($TargetPath)\$($ToolName)"
+$BackupDir = "$($TargetDir).bak"
+$ToolRepoBaseUrl = "https://github.com/usebruno/$($ToolName)"
+
+Write-Output "check updates for: $($ToolName)"
+
+$RemoteLatestTag = [System.Version]"$(git ls-remote --tags "$($ToolRepoBaseUrl).git" `
+                        | Select-String -Pattern "alpha|beta|\{\}" -NotMatch `
+                        | Sort-Object -erroraction 'SilentlyContinue' { [System.version]($_ -split 'v')[1] } `
+                        | Select-Object -Last 1
+                    )".Trim().Split('v')[1]
+
+$CurrentVersion = "0.0.0.0"
+$UpdateNotBefore = -1
+if (( Test-Path -Path $TargetDir) -And (Test-Path -Path $TargetDir\VERSION )) {
+    [string[]] $VersionFileContent = Get-Content "$($TargetDir)\VERSION"
+    # get current version
+    $CurrentVersion = [System.Version]$VersionFileContent[0].Trim()
+
+    if ( $VersionFileContent.Length -gt 1 ) {
+        $UpdateNotBefore = [int64]($VersionFileContent[1]).Trim()
+    }
+    if ( $VersionFileContent.Length -gt 2 ) {
+        $postponedUpdateVersion = [System.Version]($VersionFileContent[2]).Trim()
+    }
+} else {
+    # create new empty dir as backup fallback
+    New-Item -Path "$($TargetPath)" -Name "$($ToolName)" -ItemType "directory" -erroraction 'SilentlyContinue' > nul
+}
+
+$Now = [int64](Get-Date -AsUTC -UFormat "%s")
+# check update is needed, but will be delayed
+if ( $RemoteLatestTag -gt $CurrentVersion -And $UpdateNotBefore -eq -1 ) {
+    $NotBefore = [int64](Get-Date -AsUTC -UFormat "%s") + (24 * 3600 * $ToolUpdateDelayDays)
+    Write-Output "Update will be delayed till: $(Get-Date -AsUTC -UnixTimeSeconds $NotBefore -UFormat "%F %T")"
+    Write-Output "$($CurrentVersion)`r`n$($NotBefore)`r`n$($RemoteLatestTag)" > "$($TargetDir)\VERSION"
+}
+elseif ( $RemoteLatestTag -eq $postponedUpdateVersion -And $Now -lt $UpdateNotBefore){
+    Write-Output "Update to: $($RemoteLatestTag) is delayed till: $(Get-Date -AsUTC -UnixTimeSeconds $UpdateNotBefore -UFormat "%F %T")"
+}
+elseif ( $RemoteLatestTag -gt $postponedUpdateVersion -And $Now -lt $UpdateNotBefore){
+    $NotBefore = [int64](Get-Date -AsUTC -UFormat "%s") + (24 * 3600 * $ToolUpdateDelayDays)
+    Write-Output "Latest version: $($RemoteLatestTag) is greater then postponed: $($postponedUpdateVersion) update will be delayed till: $(Get-Date -AsUTC -UnixTimeSeconds $NotBefore -UFormat "%F %T")"
+    Write-Output "$($CurrentVersion)`r`n$($NotBefore)`r`n$($RemoteLatestTag)" > "$($TargetDir)\VERSION"
+}
+# check update is needed and is allowed
+elseif ( $RemoteLatestTag -gt $CurrentVersion -And $Now -gt $UpdateNotBefore ) {
+    Write-Output "update needed to: $($RemoteLatestTag)"
+
+    $TargetFileName = "bruno_$($RemoteLatestTag)_x64_win.zip"
+    $DownloadFullPath = "$($env:TEMP)\$($TargetFileName)"
+    $OldPath = Get-Location
+
+    $DownloadBaseUrl ="$($ToolRepoBaseUrl)/releases/download/v$($RemoteLatestTag)"
+    $Url ="$($DownloadBaseUrl)/$($TargetFileName)"
+    $CheckSumUrl="$($ToolRepoBaseUrl)/releases/expanded_assets/v$($RemoteLatestTag)"
+    $CheckSum = "$(
+                    ((Invoke-WebRequest -Uri "$($CheckSumUrl)").Content).Split("`n") `
+                        | Select-String -Pattern "$($TargetFileName)" -SimpleMatch `
+                        | Select-Object -Last 1
+                )".Split("sha256:")[1].Split("`"")[0]
+                # | Select-String -Pattern 'sha256:' -AllMatches `
+
+    if ( -not (Test-Path -Path $DownloadFullPath)  ) {
+        $ProgressPreference = 'SilentlyContinue'    # Subsequent calls do not display UI.
+        Invoke-WebRequest -Uri $Url -OutFile $DownloadFullPath
+        $ProgressPreference = 'Continue'
+    }
+
+    if ((Get-FileHash $DownloadFullPath -Algorithm SHA256).Hash.ToLower() -eq "$($CheckSum.ToLower())") {
+        Rename-Item $TargetDir $BackupDir `
+            && Write-Output "check malware" `
+            && & $vScanner -Scan -ScanType 3 -File $DownloadFullPath `
+            && Write-Output "extract" `
+            && & $7Zip x "$($DownloadFullPath)" -o"$($TargetDir)" > nul `
+            && Write-Output "$($RemoteLatestTag)" > "$($TargetDir)\VERSION" `
+            && Write-Output "cleanup" `
+            && Set-Location $OldPath `
+            && Remove-Item -Path $DownloadFullPath -Force `
+            && Remove-Item -Path $BackupDir -Recurse -Force `
+            && Write-Output "updated to version: $($RemoteLatestTag)"
+        || Rename-Item $BackupDir $TargetDir
+    }
+} else {
+    Write-Output "Nothing to update."
+}
---
+++ update-github-desktop.ps1
@@ -0,0 +1,99 @@
+# [CmdletBinding()]
+[cultureinfo]::CurrentUICulture='en-US'
+
+$TargetPath = "$($env:LOCALAPPDATA)\Programs"
+
+$7Zip = "$($TargetPath)\7-Zip\7z.exe"
+
+$vScanner = "${env:ProgramFiles}\Windows Defender\MpCmdRun.exe"
+
+$ToolName = "github-desktop"
+$ToolUpdateDelayDays = 0
+$TargetDir = "$($TargetPath)\$($ToolName)"
+$BackupDir = "$($TargetDir).bak"
+$ToolRepoBaseUrl = "https://github.com/desktop/desktop"
+
+Write-Output "check updates for: $($ToolName)"
+
+$RemoteLatestTag = [System.Version]"$(git ls-remote --tags "$($ToolRepoBaseUrl).git" `
+                        | Select-String -Pattern "alpha|beta|test|demo|\{\}" -NotMatch `
+                        | Sort-Object -erroraction 'SilentlyContinue' {($_ -split '/')[2].Replace('release-','')} `
+                        | Select-Object -Last 1
+                    )".Trim().Split('/')[2].Replace('release-', '')
+
+$CurrentVersion = "0.0.0"
+$UpdateNotBefore = -1
+if (( Test-Path -Path $TargetDir) -And (Test-Path -Path $TargetDir\VERSION )) {
+    [string[]] $VersionFileContent = Get-Content "$($TargetDir)\VERSION"
+    # get current version
+    $CurrentVersion = [System.Version]$VersionFileContent[0].Trim()
+
+    if ( $VersionFileContent.Length -gt 1 ) {
+        $UpdateNotBefore = [int64]($VersionFileContent[1]).Trim()
+    }
+    if ( $VersionFileContent.Length -gt 2 ) {
+        $postponedUpdateVersion = [System.Version]($VersionFileContent[2]).Trim()
+    }
+} else {
+    # create new empty dir as backup fallback
+    New-Item -Path "$($TargetPath)" -Name "$($ToolName)" -ItemType "directory" -erroraction 'SilentlyContinue' > nul
+}
+
+$Now = [int64](Get-Date -AsUTC -UFormat "%s")
+# check update is needed, but will be delayed
+if ( $RemoteLatestTag -gt $CurrentVersion -And $UpdateNotBefore -eq -1 ) {
+    $NotBefore = [int64](Get-Date -AsUTC -UFormat "%s") + (24 * 3600 * $ToolUpdateDelayDays)
+    Write-Output "Update will be delayed till: $(Get-Date -AsUTC -UnixTimeSeconds $NotBefore -UFormat "%F %T")"
+    Write-Output "$($CurrentVersion)`r`n$($NotBefore)`r`n$($RemoteLatestTag)" > "$($TargetDir)\VERSION"
+}
+elseif ( $RemoteLatestTag -eq $postponedUpdateVersion -And $Now -lt $UpdateNotBefore){
+    Write-Output "Update to: $($RemoteLatestTag) is delayed till: $(Get-Date -AsUTC -UnixTimeSeconds $UpdateNotBefore -UFormat "%F %T")"
+}
+elseif ( $RemoteLatestTag -gt $postponedUpdateVersion -And $Now -lt $UpdateNotBefore){
+    $NotBefore = [int64](Get-Date -AsUTC -UFormat "%s") + (24 * 3600 * $ToolUpdateDelayDays)
+    Write-Output "Latest version: $($RemoteLatestTag) is greater then postponed: $($postponedUpdateVersion) update will be delayed till: $(Get-Date -AsUTC -UnixTimeSeconds $NotBefore -UFormat "%F %T")"
+    Write-Output "$($CurrentVersion)`r`n$($NotBefore)`r`n$($RemoteLatestTag)" > "$($TargetDir)\VERSION"
+}
+# check update is needed and is allowed
+elseif ( $RemoteLatestTag -gt $CurrentVersion -And $Now -gt $UpdateNotBefore ) {
+    Write-Output "update needed to: $($RemoteLatestTag)"
+    # Write-Output "LINK: https://github.com/desktop/desktop/releases/tag/release-3.5.3"
+    # Write-Output "VERSION FULL-PATH LINK: https://github.com/desktop/desktop/releases/download/release-3.5.3/GitHubDesktop-3.5.3-x64-full.nupkg"
+
+    $TargetFileName = "GitHubDesktop-$($RemoteLatestTag)-x64-full.nupkg"
+    $DownloadFullPath = "$($env:TEMP)\$($TargetFileName)"
+    $OldPath = Get-Location
+
+    $DownloadBaseUrl ="$($ToolRepoBaseUrl)/releases/download/release-$($RemoteLatestTag)"
+    $Url ="$($DownloadBaseUrl)/$($TargetFileName)"
+    $CheckSumUrl ="$($DownloadBaseUrl)/GitHub.Desktop-$($RemoteLatestTag)-checksums.txt"
+    $CheckSum = "$([System.Text.Encoding]::UTF8.GetString((Invoke-WebRequest -Uri "$($CheckSumUrl)").Content).Split("`n") `
+                   | Select-String "$($TargetFileName)")".Split(" ")[0]
+# Write-Output "CheckSum: $CheckSum"
+
+    if ( -not (Test-Path -Path $DownloadFullPath)  ) {
+        $ProgressPreference = 'SilentlyContinue'    # Subsequent calls do not display UI.
+        Invoke-WebRequest -Uri $Url -OutFile $DownloadFullPath
+        $ProgressPreference = 'Continue'
+    }
+
+    if ((Get-FileHash $DownloadFullPath -Algorithm SHA256).Hash.ToLower() -eq "$($CheckSum.ToLower())") {
+        Write-Output "checksum: OK!"
+        Rename-Item $TargetDir $BackupDir `
+            && Write-Output "check malware" `
+            && & $vScanner -Scan -ScanType 3 -File $DownloadFullPath `
+            && Write-Output "extract" `
+            && & $7Zip x "$($DownloadFullPath)" -o"$($TargetDir).new" > nul `
+            && Move-Item "$($TargetDir).new\lib\net45" $TargetDir `
+            && Write-Output "$($RemoteLatestTag)" > "$($TargetDir)\VERSION" `
+            && Write-Output "cleanup" `
+            && Set-Location $OldPath `
+            && Remove-Item -Path $DownloadFullPath -Force `
+            && Remove-Item -Path "$($TargetDir).new" -Recurse -Force `
+            && Remove-Item -Path $BackupDir -Recurse -Force `
+            && Write-Output "updated to version: $($RemoteLatestTag)"
+        || Rename-Item $BackupDir $TargetDir
+    }
+} else {
+    Write-Output "Nothing to update."
+}
